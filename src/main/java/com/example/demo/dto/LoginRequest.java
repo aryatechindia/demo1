b55f9124aa7edca50e13a431ed95d34264e01749
Vaibhav Diwan <divaibhav@gmail.com>
2026-05-14 18:12:10 +0530

    code

---
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -0,0 +1,34 @@
+package com.example.demo.dto;
+import jakarta.validation.constraints.*;
+public class LoginRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    public LoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public LoginRequest() {
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
